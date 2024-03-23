fd4a8a83772161b39dbc2efb25e6d938aa8b92c3
Ishwor Shrestha <hello.ishwors@gmail.com>
2024-03-23 17:03:44 +0545

    Using RDS for ProsgreSQL DB Server - Added ConnectionString - DB Update

--- BankingSystem.API/DbContext/AppDBInitialize.cs
+++ BankingSystem.API/DbContext/AppDBInitialize.cs
@@ -6,7 +6,7 @@ namespace BankingSystem.API.DbContext
 {
     public static class AppDBInitialize
     {
-        public static async Task SeedUsersAndUserRolesAsync(IApplicationBuilder applicationBuilder)
+        public static async Task SeedConstantsAsync(IApplicationBuilder applicationBuilder)
         {
             using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
             {
@@ -17,12 +17,7 @@ namespace BankingSystem.API.DbContext
                 await SeedRoleAsync(roleManager, UserRoles.AccountHolder);
                 await SeedRoleAsync(roleManager, UserRoles.TellerPerson);
 
-                // Seed Users
-                await SeedUserAsync(userManager, "admin@gmail.com", "admin", "Teller Person", "Kathmandu", "9826274833", "2002-08-20T11:13:25.342Z", UserRoles.TellerPerson);
-                await SeedUserAsync(userManager, "subritiaryal13@gmail.com", "subs", "Subriti Aryal", "Gothatar, Kathmandu", "9843346520", "2002-08-20T11:13:25.342Z", UserRoles.AccountHolder);
-                await SeedUserAsync(userManager, "accountUser@gmail.com", "user", "Account Holder", "Kathmandu", "9830274849", "2000-03-23T11:13:25.342Z", UserRoles.AccountHolder);
-
-                // Save Changes
+               // Save Changes
                 var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                 await context.SaveChangesAsync();
             }
@@ -34,44 +29,6 @@ namespace BankingSystem.API.DbContext
                 await roleManager.CreateAsync(new IdentityRole<Guid>(roleName));
         }
 
-        private static async Task SeedUserAsync(UserManager<Users> userManager, string email, string username, string fullname, string address, string phoneNumber, string dateOfBirth, string role)
-        {
-            try
-            {
-                var existingUser = await userManager.FindByEmailAsync(email);
-                if (existingUser == null)
-                {
-                    var newUser = new Users
-                    {
-                        UserName = username,
-                        Fullname = fullname,
-                        Email = email,
-                        PhoneNumber = phoneNumber,
-                        Address = address,
-                        DateOfBirth = DateTime.SpecifyKind(DateTime.ParseExact(dateOfBirth, "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture), DateTimeKind.Utc),
-                        CreatedAt = DateTime.UtcNow,
-                        ModifiedAt = DateTime.UtcNow,
-                        SecurityStamp = Guid.NewGuid().ToString(),
-                        EmailConfirmed = true,
-                        PhoneNumberConfirmed = true,
-                        TwoFactorEnabled = false,
-                        LockoutEnabled = false,
-                        AccessFailedCount = 0,
-                    };
-
-                    var result = await userManager.CreateAsync(newUser, $"{username}123");
-                    if (result.Succeeded)
-                    {
-                        await userManager.AddToRoleAsync(newUser, role);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                throw new Exception(nameof(ex));
-            }
-        }
     }
 }
 
