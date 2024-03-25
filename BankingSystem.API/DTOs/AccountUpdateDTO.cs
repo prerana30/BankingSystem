b343f5998e2e4cdb3fe0a08762de7cacaec9f9e5
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-25 14:18:54 +0545

    added validation in accountUpdateDTO.cs file

--- BankingSystem.API/DTOs/AccountUpdateDTO.cs
+++ BankingSystem.API/DTOs/AccountUpdateDTO.cs
@@ -5,6 +5,8 @@ namespace BankingSystem.API.DTO
 {
     public class AccountUpdateDTO
     {
+        [Required]
+        [Range(1000, 9999, ErrorMessage = "Number must be a four-digit integer.")]
         public int AtmCardPin { get; set; }
 
         public AccountUpdateDTO()
