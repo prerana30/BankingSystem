3af92bbd1d0a6ba2966aa05c7bd884c8b6cac2c2
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-25 12:04:28 +0545

    created AccountUpdateDTO and made HttpPut request

---
+++ BankingSystem.API/DTOs/AccountUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using BankingSystem.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingSystem.API.DTO
+{
+    public class AccountUpdateDTO
+    {
+        public int AtmCardPin { get; set; }
+
+        public AccountUpdateDTO()
+        {
+
+        }
+    }
+}
