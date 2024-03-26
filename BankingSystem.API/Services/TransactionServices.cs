3339d71f4cc92d690250c4f14066339d6859dea1
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-26 15:02:04 +0545

    added email services to tranasaction deposit and withdraw

--- BankingSystem.API/Services/TransactionServices.cs
+++ BankingSystem.API/Services/TransactionServices.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using BankingSystem.API.Data.Repository;
 using BankingSystem.API.Data.Repository.IRepository;
 using BankingSystem.API.DTOs;
 using BankingSystem.API.Entities;
 using BankingSystem.API.Services.IServices;
+using BankingSystem.API.Utilities.EmailTemplates;
+using System.Security.Principal;
 
 namespace BankingSystem.API.Services
 {
@@ -10,10 +13,16 @@ namespace BankingSystem.API.Services
     {
         private readonly ITransactionRepository _transactionRepository;
         private readonly IMapper _mapper;
-        public TransactionServices(ITransactionRepository transactionRepository, IMapper mapper)
+        private readonly IEmailService _emailService;
+        private readonly IAccountRepository AccountRepository;
+        private readonly IUserRepository UserRepository;
+        public TransactionServices(ITransactionRepository transactionRepository, IMapper mapper, IEmailService emailService, IUserRepository userRepository, IAccountRepository accountRepository)
         {
             _transactionRepository = transactionRepository ?? throw new ArgumentOutOfRangeException(nameof(transactionRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
+            UserRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            AccountRepository = accountRepository;
         }
 
         public async Task<IEnumerable<Transaction>> GetTransactionsOfAccountAsync(Guid accountId)
@@ -39,12 +48,46 @@ namespace BankingSystem.API.Services
         public async Task<Transaction> DepositTransactionAsync(DepositTransactionDTO transactionDto, long accountNumber, Guid userId)
         {
             var transaction = _mapper.Map<Transaction>(transactionDto);
+
+            var user = await UserRepository.GetUserAsync(userId);
+
+            var emailBody = EmailTemplates.EmailBodyForTransactionDeposit(user.Fullname,transactionDto.Amount, transactionDto.TransactionRemarks);
+
+            // Prepare email
+            var email = new Email
+            {
+                MailSubject = "Amount Deposited",
+                MailBody = emailBody,
+                ReceiverEmail = user.Email // Use the user's email address obtained from the UserDTO
+            };
+
+            // Send email
+            await _emailService.SendEmailAsync(email);
+
             return await _transactionRepository.DepositTransactionAsync(transaction, accountNumber, userId);
         }
 
         public async Task<Transaction> WithdrawTransactionAsync(WithdrawTransactionDTO withdrawDto, long accountNumber, int atmIdAtmCardPin)
         {
             var transaction = _mapper.Map<Transaction>(withdrawDto);
+
+            var account = await AccountRepository.GetAccountByAccountNumberAsync(accountNumber);
+
+            var user = await UserRepository.GetUserAsync(account.UserId);
+
+            var emailBody = EmailTemplates.EmailBodyForTransactionDeposit(user.Fullname, withdrawDto.Amount, withdrawDto.TransactionRemarks);
+
+            // Prepare email
+            var email = new Email
+            {
+                MailSubject = "Amount Withdraw",
+                MailBody = emailBody,
+                ReceiverEmail = user.Email // Use the user's email address obtained from the UserDTO
+            };
+
+            // Send email
+            await _emailService.SendEmailAsync(email);
+
             return await _transactionRepository.WithdrawTransactionAsync(transaction, accountNumber, atmIdAtmCardPin);
         }
     }
